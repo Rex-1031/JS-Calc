ac96aafce7cf501d733a4551a611ba2789be59d7
Steve Clair <sclair@bird-technologies.com>
2021-05-26 13:42:08 -0400

    some logic added

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const calculator = document.querySelector('.container');
 
 const calcKeys = document.querySelector('.numContainer');
 
+calcKeys.addEventListener('click', e=>{
+    if(e.target.matches('button')){
+        console.log('Button Clicked!')
+    }
+})
